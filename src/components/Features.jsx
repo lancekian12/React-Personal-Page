58cd06e1b987592521faad9b439f3c4257440838
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-11-16 15:26:38 +0800

    change on the button

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react';
 import services from '../data/serviceData';
+import { useNavigate } from 'react-router-dom';
 
 function Features() {
     const [visibleIndex, setVisibleIndex] = useState(0);
+    const navigate = useNavigate();
+
 
     useEffect(() => {
         // Set a delay for each card to slide in one by one
@@ -27,11 +30,12 @@ function Features() {
                 <p className="text-textSecondary mt-4 max-w-3xl mx-auto">
                     Our Smart Electrical System Dashboard enhances the management of commercial high-rise building electrical systems through real-time monitoring, energy optimization, and predictive maintenance to ensure peak performance and sustainability.
                 </p>
-                <a href="/service">
-                    <button className="mt-6 px-8 py-3 bg-black text-white rounded-lg hover:bg-gray-800 focus:outline-none">
-                        Appoint Now
-                    </button>
-                </a>
+                <button
+                    onClick={() => navigate('/service')}
+                    className="mt-6 px-8 py-3 bg-black text-white rounded-lg hover:bg-gray-800 focus:outline-none"
+                >
+                    Appoint Now
+                </button>
             </div>
 
             <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 px-4">
